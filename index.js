c44316c2e64aef2ad6293b19a82dcab0f1828165
teresafranxman97 <teresafranxman97@gmail.com>
2020-07-28 20:51:40 -0500

    attempted to complete some uncompleted tasks

--- index.js
+++ index.js
@@ -8,30 +8,33 @@ if (votingAge > 18) {
 
 //Task b: declare a variable and then use a conditional to change the value of that variable based on the value assigned to a second variable (no function required)
 
-let permitCarryAge = 21
+let daughtersAge = 1
+  daughtersAge = daughtersAge + 1
 
-
-
+  console.log(daughtersAge);
 
 //Task c: Convert string ("1999") to integer (1999)  (no function required) // hint look up the Number method
 
-
-
+Number("1999")
 
 
 //Task d: Write a function to multiply a*b 
 
-
-
-
+function multiplyNumbers(a, b) {
+    return a * b;
+  }
+  var answer = multiplyNumbers(4,10)
+  console.log(answer);
 
 /************************************************************** Task 2 **************************************************************/
 //Age in Dog years
 //write a function that takes your age and returns it to you in dog years - they say that 1 human year is equal to seven dog years 
 
-
-
-
+function multiplyAgeInDogYrs(x, y) {
+    return x * 7;
+}
+var answer = multiplyAgeInDogYrs(22, 7)
+console.log(answer);
 
 /************************************************************** Task 3 **************************************************************/
 //Dog feeder 
@@ -41,13 +44,37 @@ let permitCarryAge = 21
 // adult dogs at least 1 year 
 // up to 5 lbs - 5% of their body weight
 // 6 - 10 lbs - 4% of their body weight 
-// 11 - 15 lbs - 3% of their body weight 
+// 11 - 15 lbs - 3% of their body weight ()
 // > 15lbs - 2% of their body weight 
 
 // Puppies less than 1 year
 // 2 - 4 months 10% of their body weight
 // 4 - 7 months 5% of their body weight 
 // 7 - 12 months 4% of their body weight
+
+function dogFeeder(weight, age) {
+    
+
+    if (age >= 0.16666667 &&age <= 0.33) {
+        food = weight * .10;
+    } else if (age >= 0.33 &&age <= 0.58333333) {
+        food = weight * .05;
+    } else if (age >= 0.58333333 &&age <= .99) {
+        food = weight * .04;
+    }
+    else if (age >= 1 &&weight <= 5){
+        food = weight * .05;
+    } else if (age >=1 && weight>= 6 &&weight <= 10){
+        food = weight * .04;
+    } else if (age >=1 && weight>= 11 &&weight<= 15){
+        food = weight * .03;
+    } else if (age >=1 && weight>=15){
+        food = weight * .02;
+    }
+    return food; 
+}
+console.log(dogFeeder(15,1));
+
 
 // when you are finished invoke your function with the weight of 15 lbs and the age of 1 year - if your calculations are correct your result should be 0.44999999999999996
   
@@ -62,17 +89,52 @@ let permitCarryAge = 21
 // use math.random to determine the computers choice 
 // hint while you can complete this with only conditionals based on strings it may help to equate choice to a number 
 
-  
-  
+let rpsGame= function (){
+    let rockGame = Math.random()
+    if (rockGame < .34 ) {
+        return 1;
+    } else if (rockGame >= .34 &&rockGame < .67) {
+        return 2;
+    } else {
+        return 3;
+    }
+}
+
+let a= rpsGame()
+
+// 1=rock 2=paper 3=sissors
+
+let rockPsGame= function(computerChoice, playChoice) {
+    if (computerChoice=== 1 && playChoice=== 1 ) {
+        console.log("Tie");
+    } else if (computerChoice=== 1 && playChoice=== 2 ) {
+        console.log("Oops try again");
+    } else if (computerChoice=== 1 && playChoice=== 3 ) {
+        console.log("You won!");
+    } else if(computerChoice=== 2 && playChoice=== 1 ) {
+            console.log("You lose");
+    } else if(computerChoice=== 2 && playChoice=== 2 ) {
+        console.log("Tie");
+    } else if(computerChoice===2 && playChoice=== 3 ) {
+        console.log("Oops try again");
+    } else if(computerChoice===3 && playChoice=== 1 ) {
+        console.log("You lose");
+    } else if(computerChoice=== 3 && playChoice=== 2 ) {
+        console.log("You win");
+    } else if(computerChoice===3 && playChoice=== 3 ) {
+        console.log("Tie");
+    }
+}
 
 /************************************************************** Task 5 **************************************************************/
 //Metric Converter
 //a. KM to Miles - should take the number of kilometers and convert it to the equal number of miles
 
+// .62 miles in a kilometer
 
-
-
-
+var kilometers = parseInt(prompt("5"));
+var miles = kilometers / 1.6;
+console.log(miles + "Miles")
 //b. Feet to CM - should take the number of feet and convert it to the equal number of centimeters
   
 
@@ -84,8 +146,7 @@ let permitCarryAge = 21
 // create a function called annoyingSong
 // the function should take a starting number as an argument and count down - at each iteration it should log (number) bottles of soda on the wall, (number) bottles of soda, take one down pass it around (number left over) bottles of soda on the wall`
   
-
-
+function annoyingSong()
 
 
 /************************************************************** Task 7 **************************************************************/
@@ -97,7 +158,11 @@ let permitCarryAge = 21
 //60s should be D 
 //and anything below 60 should be F
   
+let 
 
+    if (percent >= 0.90 &&percent < 1) {
+        console.log("A");
+    } 
   
   
 
